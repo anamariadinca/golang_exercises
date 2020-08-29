e88da812fa0e3534ab11418fd521ebe5265eab52
Ana <ana.maria.dinca.1411@gmail.com>
2020-08-29 16:46:22 +0300

    Modified function to not be exported outside the package

--- module1/find_number/find_number.go
+++ module1/find_number/find_number.go
@@ -1,10 +1,10 @@
 package module1
 
-func NumInList(numList []int, num int) bool {
+func numInList(numList []int, num int) bool {
 	for _, n := range numList {
 		if n == num {
 			return true
-		} 
-	} 
+		}
+	}
 	return false
 }
--- module1/find_number/find_number_test.go
+++ module1/find_number/find_number_test.go
@@ -1,30 +1,31 @@
 package module1
 
 import (
-	"github.com/smartystreets/goconvey/convey"
 	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
 )
 
 type input struct {
-	testNum []int
+	testNum      []int
 	searchNumber int
-	expected bool
+	expected     bool
 }
 
 var inputs = []input{
-	{[]int{2,3,5,10,22,13,4},13,true},
-	{[]int{2,3,5,10,22,13,4},15,false},
-	{[]int{1,0,-3,2,3,5,10,22,13,4},-3,true},
-	{nil, 5, false,},
-	{[]int{}, 10, false,},
+	{[]int{2, 3, 5, 10, 22, 13, 4}, 13, true},
+	{[]int{2, 3, 5, 10, 22, 13, 4}, 15, false},
+	{[]int{1, 0, -3, 2, 3, 5, 10, 22, 13, 4}, -3, true},
+	{nil, 5, false},
+	{[]int{}, 10, true},
 }
 
-func TestConveyTestFindNumber (t *testing.T) {
+func TestConveyTestFindNumber(t *testing.T) {
 	convey.Convey("For all the given tests from array", t, func() {
 
 		convey.Convey("We should obtain the same value as expected value", func() {
 			for _, input := range inputs {
-				convey.So(NumInList(input.testNum, input.searchNumber), convey.ShouldEqual, input.expected)
+				convey.So(numInList(input.testNum, input.searchNumber), convey.ShouldEqual, input.expected)
 			}
 		})
 	})
