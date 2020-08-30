37a63d6e2a7c15d0b5c3bb08baf05f54902c2687
Ana <ana.maria.dinca.1411@gmail.com>
2020-08-31 01:57:28 +0300

    exercise eight

---
+++ module1/factoring/factor.go
@@ -0,0 +1,18 @@
+package module1
+
+import "sort"
+
+func factorNumber(primes []int, num int) []int { // 2 3 5 - 30
+	var refactor []int
+	i := len(primes) - 1
+	for i >= 0 {
+		if num%primes[i] == 0 && num/primes[i] != 0 {
+			refactor = append(refactor, primes[i])
+			num = num / primes[i]
+		} else {
+			i--
+		}
+	}
+	sort.Ints(refactor)
+	return refactor
+}
---
+++ module1/factoring/factor_test.go
@@ -0,0 +1,35 @@
+package module1
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+)
+
+type input struct {
+	primes         []int
+	number         int
+	resultExpected []int
+}
+
+var tenPrimes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
+
+var inputs = []input{
+	{tenPrimes, 30, []int{2, 3, 5}},
+	{tenPrimes, 28, []int{2, 2, 7}},
+	{tenPrimes, 720, []int{2, 2, 2, 2, 3, 3, 5}},
+	{tenPrimes, 30, []int{2, 3, 5}},
+	{tenPrimes, 720, []int{2, 2, 2, 2, 3, 3, 5}},
+	{tenPrimes, 4, []int{2, 2}},
+}
+
+func TestCoveyReverseString(t *testing.T) {
+	convey.Convey("For all the given tests from array", t, func() {
+
+		convey.Convey("We should obtain the same value as expected value", func() {
+			for _, input := range inputs {
+				convey.So(factorNumber(input.primes, input.number), convey.ShouldResemble, input.resultExpected)
+			}
+		})
+	})
+}
